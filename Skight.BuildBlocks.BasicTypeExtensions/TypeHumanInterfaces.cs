011c46fea62ee0b54591e6ed2e4ae464dff54d15
Hao Wang <haowang@central1.com>
2018-09-21 14:25:36 -0700

    添加 TypeHumanInterfaces is_inherited_from()

--- Skight.BuildBlocks.BasicTypeExtensions/TypeHumanInterfaces.cs
+++ Skight.BuildBlocks.BasicTypeExtensions/TypeHumanInterfaces.cs
@@ -12,5 +12,21 @@ namespace Skight.BuildBlocks.BasicTypeExtensions
                 .OrderByDescending(x => x.GetParameters().Count())
                 .First();
         }
+        
+        public static bool is_inherited_from(this Type type, Type base_type )
+        {
+            if (base_type.IsGenericTypeDefinition)
+            {
+                foreach (var item in type.GetInterfaces())
+                {
+                    if (item.IsGenericType && item.GetGenericTypeDefinition() == base_type)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return !(type == base_type) && base_type.IsAssignableFrom(type);
+        }
+        
     }
 }
