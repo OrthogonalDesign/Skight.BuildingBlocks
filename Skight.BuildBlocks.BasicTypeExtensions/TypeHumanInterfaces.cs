ceb2b8eac972c2f130e0be827d2c7bf1b697d3b6
Wang Hao <wanghao@skight.com>
2018-09-20 23:10:53 -0700

    create solution and add TypeHumanInterfaces

---
+++ Skight.BuildBlocks.BasicTypeExtensions/TypeHumanInterfaces.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Skight.BuildBlocks.BasicTypeExtensions
+{
+    public static class TypeHumanInterfaces
+    {
+        public static ConstructorInfo greediest_constructor(this Type type)
+        {
+            return type.GetConstructors()
+                .OrderByDescending(x => x.GetParameters().Count())
+                .First();
+        }
+    }
+}
