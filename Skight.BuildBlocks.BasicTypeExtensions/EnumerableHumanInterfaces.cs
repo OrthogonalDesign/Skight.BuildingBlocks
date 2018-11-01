1dbf2a2289fd31172bb2915a27f25b056a0c3e6c
Hao Wang <haowang@central1.com>
2018-11-01 16:40:30 -0700

    Add Enumerable extensions

---
+++ Skight.BuildBlocks.BasicTypeExtensions/EnumerableHumanInterfaces.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace Skight.BuildBlocks.BasicTypeExtensions
+{
+    public static class EnumerableHumanInterfaces
+    {
+        public static void each<T>(this IEnumerable<T> source, Action<T> act)
+        {
+            foreach (var item in source)
+            {
+                act(item);
+            }
+        }
+    }
+}
